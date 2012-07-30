7c2c5e31cfc5acfd1927e4b28b537d9262d92d1b
Panos Bariamis <pbaris@gmail.com>
2012-07-30 23:46:40 +0300

    showcase project

---
+++ showcase/project/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "showcase"
+    val appVersion      = "1.0"
+
+	val appDependencies = Seq(
+    	"pbaris" %% "play2-plugins-utils" % "0.0.1",
+    	"pbaris" %% "play2-plugins-thumbs" % "1.0.0"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+		//resolvers += "pbaris" at "https://github.com/pbaris/m2-repo/raw/master"
+		resolvers += "pbaris" at "file://" + Path.userHome.absolutePath + "/.m2/repository"
+    )
+}
